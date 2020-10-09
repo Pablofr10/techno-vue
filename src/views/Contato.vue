e65b4558c935bd18f93b050d51b967b85fe9df2b
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-09 20:52:05 -0300

    Adicionando mixins.

--- src/views/Contato.vue
+++ src/views/Contato.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>Contato</h1>
+    <p>{{ api }}</p>
   </div>
 </template>
 
 <script>
+import fetchData from "@/mixins/fetchData";
 export default {
   name: "contato",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/contato");
+  },
 };
 </script>
 
--- src/views/Cursos.vue
+++ src/views/Cursos.vue
@@ -1,12 +1,18 @@
 <template>
   <div>
     <h1>Cursos</h1>
+    <p>{{ api }}</p>
   </div>
 </template>
 
 <script>
+import fetchData from "@/mixins/fetchData.js";
 export default {
   name: "cursos",
+  mixins: [fetchData],
+  created() {
+    this.fetchData("/cursos");
+  },
 };
 </script>
 
