79444c9ec46382c572219be26f4552c05962bf68
Pablo Rafael Oliveira de Araújo <pabloweb@outlook.com>
2020-10-10 00:15:08 -0300

    Finalizando projeto.

--- src/views/Aula.vue
+++ src/views/Aula.vue
@@ -6,14 +6,16 @@
     <transition>
       <div v-if="api">
         <h2>{{ api.nome }}</h2>
-        <iframe
-          width="560"
-          height="315"
-          :src="`https://www.youtube.com/embed/${api.youtube}`"
-          frameborder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowfullscreen
-        ></iframe>
+        <div class="video">
+          <iframe
+            width="560"
+            height="315"
+            :src="`https://www.youtube.com/embed/${api.youtube}`"
+            frameborder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
       </div>
     </transition>
   </div>
@@ -36,4 +38,15 @@ export default {
 </script>
 
 <style scoped>
+.video {
+  position: relative;
+  padding-bottom: 56.25%;
+}
+.video iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+}
 </style>
